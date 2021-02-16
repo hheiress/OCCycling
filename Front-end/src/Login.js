c1374f74b849cfc648c1d18f488fca5532734746
Gustavo Rossini <you@example.com>
2021-02-16 22:30:28 +0100

    Manager Required Updates

---
+++ Front-end/src/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+function Login() {
+    return (
+        <>
+            <div className="wrapper-login">
+                <div className="login">
+                <form className="form-signin">
+                    <h2 className="text-center project-name"> OCCycling</h2>
+                    <input type="text" className="form-control mb-4" name="username" placeholder="Email Address" required="" autofocus="" />
+                    <input type="password" className="form-control mb-4" name="password" placeholder="Password" required="" />
+                    <label className="checkbox">
+                        <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe" /> Remember Me
+                    </label>
+                    <Link to={'/volunteerpanel'}>
+                        <button className="btn btn-lg btn-primary btn-block mb-5" type="submit">Login as Coordinator</button>
+                    </Link>
+                   
+                </form>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Login
