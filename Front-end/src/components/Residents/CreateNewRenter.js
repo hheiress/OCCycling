cc031ce7f8b34556cc6d03702c135f7af2929517
Gustavo Rossini <you@example.com>
2021-02-18 00:35:21 +0100

    forms function handleSubmit updated

--- Front-end/src/components/Residents/CreateNewRenter.js
+++ Front-end/src/components/Residents/CreateNewRenter.js
@@ -4,8 +4,6 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import VolunteerPanel from '../VolunteerPanel';
 
-
-
 const formReducer = (state, event) => {
   if (event.reset) {
     return {
@@ -16,8 +14,10 @@ const formReducer = (state, event) => {
       passport: '',
       address: '',
       date_birth: '',
+      nationality: '',
       email: '',
       phone_number: '',
+      status: '',
     }
   }
   return {
@@ -31,18 +31,40 @@ function CreateNewRenter() {
   const [submitting, setSubmitting] = useState(false);
 
   const handleSubmit = event => {
-    const data = { formData };
-
-    fetch("http://localhost:3000/bikes", {
+    
+    const object = { 
+      "name": formData.name, 
+      "last_name": formData.last_name, 
+      "passport":formData.passport, 
+      "address": formData.address, 
+      "gender": formData.gender, 
+      "date_birth": formData.date_birth, 
+      "nationality": formData.nationality, 
+      "email": formData.email, 
+      "phone_number":formData.phone_number, 
+      "status": "Active"  
+    }
+    
+    event.preventDefault();
+    setSubmitting(true);
+    fetch("http://localhost:3000/users", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(object),
+      
     })
+
+    setTimeout(() => {
+      alert("New User Added");
+      setSubmitting(false);
+      setFormData({
+        reset: true
+      });
+      
+    }, 3000);
   }
-
-
 
   const handleChange = event => {
     setFormData({
@@ -50,6 +72,7 @@ function CreateNewRenter() {
       value: event.target.value,
     });
   }
+
   const hiddenFileInput = React.useRef(null);
   const imageUpload = event => {
     hiddenFileInput.current.click();
@@ -62,7 +85,7 @@ function CreateNewRenter() {
       <div className="wrapper">
         <div className="return-form">
           <Link to={'/residents'}>
-            <p> 	&#60; ALL RENTERS</p>
+            <p>&#60; ALL RENTERS</p>
           </Link>
         </div>
 
@@ -73,9 +96,13 @@ function CreateNewRenter() {
 
           <Form className="form-align" onSubmit={handleSubmit}>
 
-
             <div className="margin-form">
-              <button type="button" onClick={imageUpload} className="photo-btn">Photo</button>
+              
+              <button 
+              type="button" 
+              onClick={imageUpload} 
+              className="photo-btn">Photo</button>
+              
               <Form.File
                 id="userphoto"
                 name="user_photo"
@@ -83,33 +110,66 @@ function CreateNewRenter() {
                 value={formData.user_photo || ''}
                 ref={hiddenFileInput}
                 style={{ display: 'none' }}
-
               />
             </div>
 
             <div className="wrap-names-renter">
 
               <div className="margin-form-name">
-                <Form.Control name="name" autocomplete="off" onChange={handleChange} value={formData.name || ''} placeholder="Name" />
+                <Form.Control 
+                name="name" 
+                autocomplete="off" 
+                onChange={handleChange} 
+                value={formData.name || ''}
+                placeholder="Name"
+                required 
+                />
               </div>
 
               <div className="margin-form-name">
-                <Form.Control name="last_name" autocomplete="off" onChange={handleChange} value={formData.last_name || ''} placeholder="Last Name" />
+                <Form.Control 
+                name="last_name" 
+                autocomplete="off" 
+                onChange={handleChange} 
+                value={formData.last_name || ''} 
+                placeholder="Last Name"
+                required
+                />
               </div>
 
             </div>
 
             <div className="margin-form">
-              <Form.Control name="passport" autocomplete="off" onChange={handleChange} value={formData.passport || ''} placeholder="Passport" />
+              <Form.Control 
+              name="passport" 
+              autocomplete="off" 
+              onChange={handleChange} 
+              value={formData.passport || ''} 
+              placeholder="Passport"
+              required 
+              />
             </div>
 
             <div className="margin-form">
-              <Form.Control name="address" autocomplete="off" onChange={handleChange} value={formData.address || ''} placeholder="Address" />
+              <Form.Control 
+              name="address" 
+              autocomplete="off" 
+              onChange={handleChange} 
+              value={formData.address || ''} 
+              placeholder="Address"
+              required 
+              />
             </div>
 
             <div className="margin-form condition">
               <p>GENDER</p>
-              <Form.Control as="select" className="select" name="gender" onChange={handleChange} value={formData.gender || ''}>
+              <Form.Control 
+              as="select" 
+              className="select" 
+              name="gender" 
+              onChange={handleChange} 
+              value={formData.gender || ''}
+              required >
 
                 <option value="" disabled selected hidden>Choose...</option>
 
@@ -122,19 +182,52 @@ function CreateNewRenter() {
 
             <div className="margin-form condition">
               <p>DATE OF BIRTH</p>
-              <Form.Control type="date" name="date_birth" autocomplete="off" onChange={handleChange} value={formData.date_birth || ''} placeholder="Date of Birth" />
+              <Form.Control 
+              type="date" 
+              name="date_birth" 
+              autocomplete="off" 
+              onChange={handleChange} 
+              value={formData.date_birth || ''} 
+              placeholder="Date of Birth"
+              required
+               />
+            </div>
+
+            <div className="margin-form condition">
+              <Form.Control 
+              name="nationality" 
+              autocomplete="off" 
+              onChange={handleChange} 
+              value={formData.nationality || ''} 
+              placeholder="Nationality"
+              required />
             </div>
 
             <div className="margin-form">
-              <Form.Control type="email" name="email" autocomplete="off" onChange={handleChange} value={formData.email || ''} placeholder="Email" />
+              <Form.Control 
+              type="email" 
+              name="email" 
+              autocomplete="off" 
+              onChange={handleChange} 
+              value={formData.email || ''} 
+              placeholder="Email"
+              required />
             </div>
 
             <div className="margin-form">
-              <Form.Control type="tel" name="phone_number" autocomplete="off" onChange={handleChange} value={formData.phone_number || ''} placeholder="Phone Number" />
+              <Form.Control className="number" 
+              type="number" 
+              name="phone_number"
+              min="-2147483648"
+              max= "2147483647"
+              onChange={handleChange} 
+              value={formData.phone_number || ''} 
+              placeholder="Phone Number"
+              required />
             </div>
 
             <div className="margin-form-button">
-              <Button className="submit-button" type="submit" disabled={submitting}>Register</Button>
+              <Button className="submit-button" type="submit">Register</Button>
             </div>
 
           </Form>
