c2c59cd3d582b9b67e2127106bb17a1bef37db47
Diana Dashkovskaya <dianadashkovskaya@MacBook-Pro-Diana.local>
2021-02-17 13:08:47 +0100

    new homepage with login and signup forms

---
+++ Front-end/src/components/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function HomePage() {
+    return (
+        <>
+            <div className="wrapper-login">
+                <div className="login">
+                <div className="homepage-block">
+                    <img src="/images/icon.svg" className="img" />
+                    <h2 className="text-center homepage-name"> OCCycling</h2>
+                    <Link to={'/login'}>
+                        <button className="btn btn-lg btn-primary submit-button btn-block" type="submit">Login </button>
+                    </Link>
+                    <Link to={'/signup'}>
+                        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
+                    </Link>
+                    <p className="text-center homepage-text"> START YOUR WAY WITH US!</p>
+                </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default HomePage;
