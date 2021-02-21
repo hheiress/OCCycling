5095ff2bbba95316d1fd9a12da2c4fc041027667
Jose-cod7 <josegarriaga7@gmail.com>
2021-02-21 11:47:23 +0100

    Login-register connected frontend / notification added

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,10 +1,14 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect
 } from "react-router-dom";
 
 import HomePage from './components/HomePage';
@@ -19,14 +23,63 @@ import AddTime from './components/AddTime';
 import AllBicycles from './components/Bicycles/Bicycles';
 import AddNewBicycle from './components/Bicycles/AddNewBicycle';
 import CreateNewRenter from './components/Residents/CreateNewRenter';
+import Dashboard from './components/Dashboard'; /* Add 19/02 */
 
+toast.configure()
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated (boolean);
+  };
+
+  async function isAuth () {
+    try {
+
+      const response = await fetch("http://localhost:3001/auth/verify", {
+        method: "GET",
+        headers: { token: localStorage.token }
+      });
+
+      const parseRes = await response.json()
+      console.log(parseRes)
+
+      parseRes === true ? setIsAuthenticated(true) :
+      setIsAuthenticated(false);
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    isAuth()
+  })
+
   return (
     <Router>
       <Switch>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/signup" component={SignUp}/>
+      <Route exact path="/login" 
+         render = {props => 
+          !isAuthenticated ? ( <Login {...props} setAuth = {setAuth} /> ) : 
+            ( <Redirect to = "/dashboard" /> ) 
+        } 
+      />
+      <Route exact path="/signup"
+        render = {props => 
+          !isAuthenticated ? ( <SignUp {...props} setAuth = {setAuth} /> ) :
+            ( <Redirect to = "/dashboard" /> )
+       }
+     /> 
+      <Route exact path="/dashboard" 
+        render = {props => 
+          isAuthenticated ? ( <Dashboard {...props} setAuth = {setAuth} /> ) :
+           ( <Redirect to = "/login" /> )
+       }
+      />  {/* Add 19/02 */}
+
         {/* Volunteers */}
         {/* Volunteers */}
         <Route exact path="/rentbicycle" component={RentBicycle} />
