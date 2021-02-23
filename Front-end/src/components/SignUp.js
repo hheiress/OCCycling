fc58eb774a1aeb108ead5cd06dd328ba17aa488c
Jose-cod7 <josegarriaga7@gmail.com>
2021-02-23 16:58:18 +0100

    localhost change

--- Front-end/src/components/SignUp.js
+++ Front-end/src/components/SignUp.js
@@ -20,7 +20,7 @@ function SignUp({setAuth}) {
         try {
             //storage in our DB
             const body = {name, email, password}
-            const response = await fetch("http://localhost:3001/auth/register", {
+            const response = await fetch("http://localhost:3000/auth/register", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(body)
