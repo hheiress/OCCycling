f3196320e97774d975003e1a965e844f28654079
ric <ricard.more@weareadaptive.com>
2021-10-28 16:28:55 +0200

    Ric: set a fixed port in back end

--- back-end/server.js
+++ back-end/server.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const routes = require("./routes.js");
 const pool = require("./db.js");
 
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 app.use(cors());
